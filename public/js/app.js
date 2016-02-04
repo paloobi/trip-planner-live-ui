fba21c0af1c19401b37c0377774e98a0583fe055
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-02-04 17:33:47 -0500

    places can be added, only one hotel can be in a day's itinerary at a time, can't add dupes

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,66 @@
 $(document).ready( function() {
 
   $('.hotelList button').on('click', function() {
-    var currHotel = $('.hotelList select').val();
-    console.log('hotel to add:', currHotel);
+    var $selectHotel = $('.hotelList select').val();
+    var $currHotel = $('.currHotel ul');
+
+    var $newHotel = $('<div class="itinerary-item">' +
+                    '<span class="title">' + $selectHotel + '</span>' +
+                    '<button class="btn btn-xs btn-danger remove btn-circle">x</button></div>')
+    if (!$currHotel.children().length) {
+      $currHotel.append($newHotel);
+    } else {
+      $currHotel.children().remove();
+      $currHotel.append($newHotel);
+    }
+
   });
 
   $('.restaurantList button').on('click', function() {
-    var currRestaurant = $('.restaurantList select').val();
+    var $selectRestaurant = $('.restaurantList select').val();
+    var $currRestaurants = $('.currRestaurants ul');
+
+    var $currRestaurantTitles = $('.currRestaurants span.title');
+
+    var doesExist = false;
+    for (var i = 0; i < $currRestaurantTitles.length; i++) {
+      // why did we have to cast this iterated item into a jQuery object?
+      if ( $selectRestaurant === $( $currRestaurantTitles[i] ).text() ) {
+        doesExist = true;
+        break;
+      }
+    }
+
+    if (!doesExist) {
+      var $newRestaurant = $('<div class="itinerary-item">' +
+                      '<span class="title">' + $selectRestaurant + '</span>' +
+                      '<button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+      $currRestaurants.append($newRestaurant);
+    }
+
   });
 
   $('.activityList button').on('click', function() {
-    var currActivity = $('.activityList select').val();
+    var $selectActivity = $('.activityList select').val();
+    var $currActivities = $('.currActivities ul');
+
+    var $currActivityTitles = $('.currActivities span.title');
+
+    var doesExist = false;
+    for (var i = 0; i < $currActivityTitles.length; i++) {
+      if ( $selectActivity === $( $currActivityTitles[i] ).text() ) {
+        doesExist = true;
+        break;
+      }
+    }
+
+    if (!doesExist) {
+      var $newActivity = $('<div class="itinerary-item">' +
+                      '<span class="title">' + $selectActivity + '</span>' +
+                      '<button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+      $currActivities.append($newActivity);
+    }
+
   });
 
 })
