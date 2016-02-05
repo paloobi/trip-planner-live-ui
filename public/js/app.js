c22839b37540bdc273a19aab88b302d255e14694
leilabl <leilaloezer@gmail.com>
2016-02-05 12:02:43 -0500

    Refactor code to be object oriented and add and switch days functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,77 +1,130 @@
 $(document).ready( function() {
 
-  $('.hotelList button').on('click', function() {
-    var selectHotel = $('.hotelList select').val();
-    var $currHotel = $('.currHotel ul');
+  // location LIST, can be a hotel, restaurants or activities list
+  // handles adding and removing items from DOM
+  function LocationList(type) {
+    this.type = type;
+    this.contents = [];
+  }
 
-    var $newHotel = $('<div class="itinerary-item">' +
-                    '<span class="title">' + selectHotel + '</span>' +
-                    '<button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
-    if (!$currHotel.children().length) {
-      $currHotel.append($newHotel);
-    } else {
-      $currHotel.children().remove();
-      $currHotel.append($newHotel);
+  // add a new item to this list
+  LocationList.prototype.add = function () {
+    var selectItem = $('.' + this.type +'-list select').val();
+
+
+    if (this.type === 'hotel' && this.contents.length) {
+      this.contents = [];
     }
+    var newItem = '<div class="itinerary-item">' +
+                '<span class="title">' + selectItem + '</span>' +
+                '<button class="btn btn-xs btn-danger remove btn-circle">x</button></div>';
+    if (this.contents.indexOf(newItem) === -1 ) {
+      this.contents.push(newItem);
+      this.show();
+    }
+  } 
 
+  // display this list in the DOM
+  LocationList.prototype.show = function () {
+    var $currItems = $('.curr-' + this.type + ' ul');
+    $currItems.html( this.contents.join("") );
+  }
+
+  // remove an item from the list
+  LocationList.prototype.delete = function ($item) {
+    var name = $item.siblings('.title').text();
+    var index = this.contents.indexOf(name);
+    this.contents.splice(index, 1);
+    $item.parent().remove();
+  } 
+
+  // DAY containing a list each for hotel, restaurants, activities
+  function Day(number) {
+    this.day = number;
+    this.hotel = new LocationList('hotel');
+    this.restaurants = new LocationList('restaurants');
+    this.activities = new LocationList('activities');
+  }
+
+  // show the day's lists
+  Day.prototype.show = function () {
+    this.hotel.show();
+    this.restaurants.show();
+    this.activities.show();
+  }
+
+  // ITINERARY containing a list of days
+  function Itinerary() {
+    this.days = [new Day(1)];
+  }
+
+  // add a day to the itinerary
+  Itinerary.prototype.addDay = function(num) {
+    this.days.push(new Day(num));
+
+  }
+
+  // remove a day from the itinerary
+  Itinerary.prototype.removeDay = function() {
+
+  }
+
+  // instantiate a single itinerary instance
+  var currItinerary = new Itinerary();
+
+
+  function getCurrentDay() {
+    return $('.current-day').text();
+  }
+  $('.hotel-list button').on('click', function() {
+    currItinerary.days[getCurrentDay() - 1].hotel.add();
   });
 
-  $('.restaurantList button').on('click', function() {
-    var selectRestaurant = $('.restaurantList select').val();
-    var $currRestaurants = $('.currRestaurants ul');
-
-    var currRestaurantTitles = $('.currRestaurants span.title').map(function(idx, val) {
-      return $(val).text();
-    }).get();
-    
-    if ( currRestaurantTitles.indexOf(selectRestaurant) === -1 ) {
-      var $newRestaurant = $('<div class="itinerary-item">' +
-                      '<span class="title">' + selectRestaurant + '</span>' +
-                      '<button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
-      $currRestaurants.append($newRestaurant);
-    }
-
+  $('.restaurants-list button').on('click', function() {
+    currItinerary.days[getCurrentDay() - 1].restaurants.add();
   });
 
-  $('.activityList button').on('click', function() {
-    var selectActivity = $('.activityList select').val();
-    var $currActivities = $('.currActivities ul');
-
-    var currActivityTitles = $('.currActivities span.title').map(function(idx, val) {
-      return $(val).text();
-    }).get();
-
-    if (currActivityTitles.indexOf(selectActivity) === -1) {
-      var $newActivity = $('<div class="itinerary-item">' +
-                      '<span class="title">' + selectActivity + '</span>' +
-                      '<button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
-      $currActivities.append($newActivity);
-    }
-
+  $('.activities-list button').on('click', function() {
+    currItinerary.days[getCurrentDay() - 1].activities.add();
   });
 
-  // $('ul .remove').on('click', function() {
-  //   console.log(this);
-  // });
-
-  $('.list-group').on('click', '.itinerary-item button.remove', function() { 
-    $(this).parent().remove();
+  $('.list-group').on('click', '.itinerary-item button.remove', function() {
+    var $item = $(this);
+    var listType = $item.parent().parent().parent().attr('class');
+    listType = listType.slice(listType.indexOf('-') + 1);
+    if (listType === 'hotel') {
+      hotel.delete($item);
+    } else if (listType === 'restaurants') {
+      restaurants.delete($item);
+    } else if (listType === 'activities') {
+      activities.delete($item);
+    }
   });
 
   $('.add-day').on('click', function() {
     //find value of last sibling
+
     var lastDay = Number($(this).siblings().last().text()) + 1;
-    console.log(lastDay);
+    currItinerary.addDay(lastDay);
 
     var newDay = '<button class="btn btn-circle day-btn">'+ lastDay +'</button>';
 
     $(newDay).insertBefore($(this));
 
-    //create variable
+  });
 
-    //append button with new value
+  $('.day-buttons').on('click', '.day-btn', function() {
+    var oldDay = $('.current-day');
 
+    if (!$(this).hasClass('add-day') ) {
+      oldDay.removeClass('current-day');
+      
+      var newDay = $(this);
+      newDay.addClass('current-day');
 
-  })
+      currItinerary.days[Number(newDay.text()) - 1].show();
+    }
+
+  });
 
 });
